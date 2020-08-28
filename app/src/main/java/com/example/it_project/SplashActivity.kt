f2b83c27f46fd2ff15f8533050e06508166b4e49
glassnekeep <glassnekeep@yandex.ru>
2020-08-28 18:30:41 +0300

    Added java class ActivityUtilities, special .kt file for constants, animation file for SplashActvity and added this animation to SplashActivity

--- app/src/main/java/com/example/it_project/SplashActivity.kt
+++ app/src/main/java/com/example/it_project/SplashActivity.kt
@@ -2,10 +2,46 @@ package com.example.it_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SplashActivity : AppCompatActivity() {
+
+    private var imageView: ImageView? = null
+    private var animation: Animation? = null
+    private var progressBar: ProgressBar? = null
+    private var layout: ConstraintLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        layout = findViewById<View>(R.id.splashLayout) as ConstraintLayout
+        imageView = findViewById<View>(R.id.ivSplashIcon) as ImageView
+        animation = AnimationUtils.loadAnimation(baseContext, R.anim.rotate)
+        progressBar = findViewById<View>(R.id.progressBar) as ProgressBar
+    }
+
+    private fun initFunctionality() {
+        layout!!.postDelayed( {
+            progressBar!!.visibility = View.GONE
+            imageView!!.startAnimation(animation)
+            animation!!.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation) {}
+                override fun onAnimationEnd(animation: Animation) {
+                    ActivityUtilities.getInstance().invokeNewActivity(this@SplashActivity, MainActivity::class.java, true)
+                }
+
+                override fun onAnimationRepeat(animation: Animation) {}
+            })
+        }, SPLASH_DURATION.toLong())
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initFunctionality()
     }
 }
---
+++ app/src/main/java/com/example/it_project/appConstants.kt
@@ -0,0 +1,6 @@
+package com.example.it_project
+
+/*Данный файл содержит все константы приложения*/
+
+lateinit var APP_ACTIVITY: MainActivity
+const val SPLASH_DURATION: Int = 2500
