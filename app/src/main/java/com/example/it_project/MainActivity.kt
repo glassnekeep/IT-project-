1056955b7ac6a328b791046157120663cc398b0b
glassnekeep <glassnekeep@yandex.ru>
2020-09-01 11:41:59 +0300

    Started to create toolbar, header and Drawer

--- app/src/main/java/com/example/it_project/MainActivity.kt
+++ app/src/main/java/com/example/it_project/MainActivity.kt
@@ -1,11 +1,101 @@
 package com.example.it_project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var toolbar: Toolbar? = null
+    private var header: AccountHeader? = null
+    private var drawer: Drawer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        var profile = ProfileDrawerItem().withIcon(R.drawable.ic_dev)
+
+        createHeader()
+        createDrawer()
     }
+
+    private fun createHeader() {
+        header = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .withTranslucentStatusBar(true)
+            .addProfiles(
+                ProfileDrawerItem().withName("Kirill Legkodukh")
+                    .withEmail("glassnekeep@yandex.ru")
+            ).build()
+    }
+
+    private fun createDrawer() {
+        drawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(toolbar!!)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(header!!)
+            .addDrawerItems(
+                PrimaryDrawerItem().withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать группу")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_baseline_add_24),
+                PrimaryDrawerItem().withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать секретный чат")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_baseline_bar_chart_24),
+                PrimaryDrawerItem().withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName("Создать канал")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_baseline_group_24),
+                PrimaryDrawerItem().withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName("Контакты")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_baseline_group_add_24),
+                PrimaryDrawerItem().withIdentifier(104)
+                    .withIconTintingEnabled(true)
+                    .withName("Звонки")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_baseline_history_24),
+                PrimaryDrawerItem().withIdentifier(105)
+                    .withIconTintingEnabled(true)
+                    .withName("Избранное")
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_baseline_settings_24),
+            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(view: View?, position: Int, drawerItem: IDrawerItem<*>): Boolean {
+                    when(position){
+                        1 -> ActivityUtilities.getInstance().invokeNewActivity(this@MainActivity, SplashActivity::class.java, true)
+                    }
+                    return false
+                }
+            })
+            /*.withSavedInstance(savedInstanceState)
+            *.withShowDrawerOnFirstLaunch(true)
+            *.withShowDrawerUntilDraggedOpened(true)
+            */.build()
+    }
+
+
 }
